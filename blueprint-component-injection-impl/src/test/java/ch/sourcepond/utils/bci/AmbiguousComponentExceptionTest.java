dfe3799de6be7dab8c280a12fd7446bd2c3df917
Roland Hauser <sourcepond@gmail.com>
2016-10-10 12:17:16 +0200

    Fixed message concatenation + fixed test

--- blueprint-component-injection-impl/src/test/java/ch/sourcepond/utils/bci/AmbiguousComponentExceptionTest.java
+++ blueprint-component-injection-impl/src/test/java/ch/sourcepond/utils/bci/AmbiguousComponentExceptionTest.java
@@ -1,6 +1,6 @@
 package ch.sourcepond.utils.bci;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,9 +15,9 @@ public class AmbiguousComponentExceptionTest {
 		ambiguousComponents.put("componentId1", new Object());
 		ambiguousComponents.put("componentId2", new Object());
 		final AmbiguousComponentException ex = new AmbiguousComponentException("anyField", ambiguousComponents);
-		assertEquals(
-				"There are more than one component which match field anyField. Following components have been found:\n"
-						+ "\tcomponentId1 -> class java.lang.Object\n" + "\tcomponentId2 -> class java.lang.Object\n",
-				ex.getMessage());
+		assertTrue(ex.getMessage().startsWith(
+				"There are more than one component which match field anyField. Following components have been found:\n"));
+		assertTrue(ex.getMessage().contains("\tcomponentId1 -> java.lang.Object@"));
+		assertTrue(ex.getMessage().contains("\tcomponentId2 -> java.lang.Object@"));
 	}
 }
