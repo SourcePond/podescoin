a6a02ebc52c92a55fb26ea9809980d5199920a85
Roland Hauser <sourcepond@gmail.com>
2016-11-30 17:47:31 +0100

    Added some tests, added licenses

--- podescoin-core/src/test/java/ch/sourcepond/utils/podescoin/internal/WriteReadObjectTest.java
+++ podescoin-core/src/test/java/ch/sourcepond/utils/podescoin/internal/WriteReadObjectTest.java
@@ -10,11 +10,16 @@ See the License for the specific language governing permissions and
 limitations under the License.*/
 package ch.sourcepond.utils.podescoin.internal;
 
+import static ch.sourcepond.utils.podescoin.internal.WriteReadObjectTest.ClassWithReadAndWriteInjectorMethod.TEST_STRING;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
@@ -22,8 +27,11 @@ import org.junit.Test;
 
 import ch.sourcepond.utils.podescoin.ClassVisitorTest;
 import ch.sourcepond.utils.podescoin.TestComponent;
+import ch.sourcepond.utils.podescoin.api.ReadObject;
 import ch.sourcepond.utils.podescoin.api.Recipient;
 import ch.sourcepond.utils.podescoin.api.WriteObject;
+import ch.sourcepond.utils.podescoin.internal.util.PodesCoinClassLoader;
+import ch.sourcepond.utils.podescoin.internal.util.PodesCoinObjectInputStream;
 
 /**
  *
@@ -84,5 +92,41 @@ public class WriteReadObjectTest extends ClassVisitorTest {
 
 		// defaultReadObject should have been called.
 		verify(objOutStream).defaultWriteObject();
+	}
+
+	@Recipient
+	public static class ClassWithReadAndWriteInjectorMethod implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		public static final String TEST_STRING = "testString";
+		public String testString;
+
+		@ReadObject
+		void readObject(final ObjectInputStream in, final TestComponent pTestComponent) throws IOException {
+			testString = in.readUTF();
+		}
+
+		@WriteObject
+		void writeObject(final ObjectOutputStream out, final TestComponent pTestComponent) throws IOException {
+			out.writeUTF(TEST_STRING);
+		}
+	}
+
+	@Test
+	public void transferString() throws Exception {
+		final PodesCoinClassLoader loader = new PodesCoinClassLoader();
+		final Class<?> cl = loader.loadClass(ClassWithReadAndWriteInjectorMethod.class.getName());
+		final Object obj = cl.newInstance();
+		final ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		try (final ObjectOutputStream out = new ObjectOutputStream(bout)) {
+			out.writeObject(obj);
+		}
+		try (final ObjectInputStream in = new PodesCoinObjectInputStream(loader,
+				new ByteArrayInputStream(bout.toByteArray()))) {
+			final Object clone = in.readObject();
+			assertEquals(TEST_STRING, cl.getField("testString").get(clone));
+		}
 	}
 }
